302f84cf283095ef444f7ba9ad0b8766ffbca4ec
st191442 <st191442@fww-nbeans-0967.fww.hs-wismar.de>
2021-01-27 00:04:13 +0100

    Asynchrone Methode

---
+++ pokedex/pokedex-ejb/src/main/java/de/kubbillum/wings/pokedex/ejb/interfaces/MessageSender.java
@@ -0,0 +1,11 @@
+package de.kubbillum.wings.pokedex.ejb.interfaces;
+
+import java.util.concurrent.Future;
+
+import de.kubbillum.wings.pokedex.persistence.entities.PokedexUser;
+
+public interface MessageSender {
+
+	public Future<Boolean> send(String subject, String body, PokedexUser user);
+	
+}
