2e38c0e43157bbe4824d1b183d2376118d63a107
st191442 <st191442@fww-nbeans-0967.fww.hs-wismar.de>
2021-01-26 23:11:31 +0100

    !!! Running JMS Sender and Receiver !!! BirthdayNotificationsReceiver; BirthdayNotificationBean

--- pokedex/pokedex-ejb/src/main/java/de/kubbillum/wings/pokedex/ejb/beans/BirthdayNotificationBean.java
+++ pokedex/pokedex-ejb/src/main/java/de/kubbillum/wings/pokedex/ejb/beans/BirthdayNotificationBean.java
@@ -1,22 +1,29 @@
-//package de.kubbillum.wings.pokedex.ejb.beans;
-//
-//import java.util.List;
-//
-//import javax.annotation.PostConstruct;
-//import javax.annotation.Resource;
-//import javax.ejb.EJB;
-//import javax.ejb.Schedule;
-//import javax.ejb.Startup;
-//import javax.inject.Inject;
-//import javax.jms.Destination;
-//import javax.jms.JMSContext;
-//import javax.jms.JMSDestinationDefinition;
-//import javax.jms.JMSDestinationDefinitions;
-//import javax.ejb.Singleton;
-//
-//import de.kubbillum.wings.pokedex.ejb.interfaces.PokedexUserDAO;
-//import de.kubbillum.wings.pokedex.persistence.entities.PokedexUser;
-//
+package de.kubbillum.wings.pokedex.ejb.beans;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.annotation.Resource;
+import javax.ejb.EJB;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.inject.Inject;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.json.Json;
+import javax.json.JsonObject;
+
+import de.kubbillum.wings.pokedex.ejb.interfaces.PokedexUserDAO;
+import de.kubbillum.wings.pokedex.persistence.entities.PokedexUser;
+
 //@Singleton
 //@Startup
 //@JMSDestinationDefinitions({
@@ -48,3 +55,53 @@
 //	}
 //
 //}
+
+@Singleton
+public class BirthdayNotificationBean {
+
+	@Resource(lookup = "java:/ConnectionFactory")
+	private ConnectionFactory jmsFactory;
+
+	@Resource(lookup = "java:/jms/queue/exampleQueue")
+	private Queue jmsQueue;
+
+	@EJB
+	private PokedexUserDAO pokedexUserDAO;
+
+	@Inject
+	private JMSContext context;
+
+	@Schedule(hour = "*", minute = "*", second = "*/30", persistent = false)
+	public void sendStockInformation() {
+		System.out.println("BirthdayNotificationBean...");
+		TextMessage message;
+
+		try (Connection connection = jmsFactory.createConnection();
+				Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+				MessageProducer producer = session.createProducer(jmsQueue)) {
+
+//			JsonObject stockInformation = Json.createObjectBu#ilder()
+//					.add("stockCode", stockCodes[ThreadLocalRandom.current().nextInt(stockCodes.length)])
+//					.add("price", ThreadLocalRandom.current().nextDouble(1.0, 150.0))
+//					.add("timestamp", Instant.now().toEpochMilli()).build();
+
+			// List<PokedexUser> birthdays = pokedexUserDAO.getUsersHavingBirthday();
+		List<PokedexUser> birthdays = pokedexUserDAO.getAllPokedexUsers();
+
+		for (PokedexUser user : birthdays) {
+			//context.createProducer().send(birthdayDestination, user);
+			ObjectMessage om = session.createObjectMessage();
+			om.setObject(user);
+			producer.send(om);
+		}
+//			message = session.createTextMessage();
+//			message.setText("BirthdayNotificationBean...");
+//
+//			producer.send(message);
+
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
--- pokedex/pokedex-ejb/src/main/java/de/kubbillum/wings/pokedex/ejb/beans/InitializationBean.java
+++ pokedex/pokedex-ejb/src/main/java/de/kubbillum/wings/pokedex/ejb/beans/InitializationBean.java
@@ -32,6 +32,7 @@ public class InitializationBean {
 			user.setLastName("Mustermann");
 			user.setGender(Gender.Male);
 			user.setUserName("m.kubbillum");
+			user.setEmailAdress("m.kubbillum@stud.hs-wismar.de");
 			user.setBirthday(LocalDate.parse("1984-01-25"));
 			PokedexUser resultUser = pokedexUserDAO.create(user);
 			
