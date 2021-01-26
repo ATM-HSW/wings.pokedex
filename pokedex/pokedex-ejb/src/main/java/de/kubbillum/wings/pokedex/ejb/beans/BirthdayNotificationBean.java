ba59be92eeec692678c8118d87bc3e41d18272f8
st191442 <st191442@fww-nbeans-0967.fww.hs-wismar.de>
2021-01-26 23:16:26 +0100

    Cleaning

--- pokedex/pokedex-ejb/src/main/java/de/kubbillum/wings/pokedex/ejb/beans/BirthdayNotificationBean.java
+++ pokedex/pokedex-ejb/src/main/java/de/kubbillum/wings/pokedex/ejb/beans/BirthdayNotificationBean.java
@@ -24,38 +24,6 @@ import javax.json.JsonObject;
 import de.kubbillum.wings.pokedex.ejb.interfaces.PokedexUserDAO;
 import de.kubbillum.wings.pokedex.persistence.entities.PokedexUser;
 
-//@Singleton
-//@Startup
-//@JMSDestinationDefinitions({
-//		@JMSDestinationDefinition(name = "java:/jms/queue/exampleQueue", interfaceName = "javax.jms.Queue") })
-//public class BirthdayNotificationBean {
-//
-////	@EJB
-////	private PokedexUserDAO pokedexUserDAO;
-//
-//	@Inject
-//	private JMSContext context;
-//
-//	// @Resource(lookup = "java:/jms/queue/exampleQueue")
-//
-//	//@Resource(lookup = "java:/jms/queue/exampleQueue")
-//	@Resource(mappedName = "java:/jms/queue/exampleQueue")	
-//	private Destination birthdayDestination;
-//
-//	@PostConstruct
-//	@Schedule(hour = "*", minute = "*", second = "*/30", persistent = false)
-//	private void checkBirthday() {
-//		context.createProducer().send(birthdayDestination, "user");
-//		// List<PokedexUser> birthdays = pokedexUserDAO.getUsersHavingBirthday();
-////		List<PokedexUser> birthdays = pokedexUserDAO.getAllPokedexUsers();
-//
-////		for (PokedexUser user : birthdays) {
-////			context.createProducer().send(birthdayDestination, user);
-////		}
-//	}
-//
-//}
-
 @Singleton
 public class BirthdayNotificationBean {
 
