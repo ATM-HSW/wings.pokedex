c372bfc5c34a5a0b4f49294f153d9fe5285457d4
st191442 <st191442@fww-nbeans-0967.fww.hs-wismar.de>
2021-02-01 20:30:05 +0100

    Backup Suche

--- pokedex/pokedex-web/src/ngfrontend/src/app/full-pokedex/full-pokedex.component.ts
+++ pokedex/pokedex-web/src/ngfrontend/src/app/full-pokedex/full-pokedex.component.ts
@@ -5,11 +5,55 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './full-pokedex.component.html',
   styleUrls: ['./full-pokedex.component.css']
 })
+
 export class FullPokedexComponent implements OnInit {
 
   constructor() { }
+  pokemons: any;
+
+  textValue = 'initial value';
 
   ngOnInit(): void {
+    const Pokedex = require('pokeapi-js-wrapper');
+
+    const P = new Pokedex.Pokedex();
+    P.resource([
+      "https://pokeapi.co/api/v2/pokemon/?limit=898&offset=0"
+    ]).then((response: any) => {
+      console.log(response);
+      this.pokemons = response[0].results;
+    })
   }
 
+  searchByNrName(value: string): void {
+    document.querySelectorAll('.pokemons').forEach(element => element.classList.remove('pokemon-selected'));
+
+    console.log('value: ' + value);
+    var searchNrName: HTMLElement;
+    // searchNrName = document.getElementById("searchNrName");
+    if (value.length == 0) {
+      alert("Bitte geben Sie den Namen oder die Nummer des Pokemon ein!")
+      return;
+    }
+    value = value.toLocaleLowerCase();
+    var element;// : HTMLElement;
+    // if (isNaN(parseInt(value))) {
+    //   element = document.getElementsByClassName("pokemon-" + value)[0];
+    // } else {
+    //   element = document.getElementById("pokemon-" + value);
+    // }
+    element = document.getElementsByClassName("pokemon-" + value)[0];
+    if (element != null) {
+      element.classList.add("pokemon-selected");
+      //element.scrollIntoView(); LARVESTA
+      const yOffset = -20;
+      const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({ top: y, behavior: 'smooth' })
+
+
+    } else {
+      alert("Zu dem Namen oder der Nummer wurde keine Pokemon gefunden.")
+    }
+  }
+  
 }
