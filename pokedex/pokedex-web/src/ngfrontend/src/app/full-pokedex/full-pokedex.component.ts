038464d1db700fda39b02b5c36e5732b86d4c23a
st191442 <st191442@fww-nbeans-0967.fww.hs-wismar.de>
2021-02-19 21:12:19 +0100

    Backup

--- pokedex/pokedex-web/src/ngfrontend/src/app/full-pokedex/full-pokedex.component.ts
+++ pokedex/pokedex-web/src/ngfrontend/src/app/full-pokedex/full-pokedex.component.ts
@@ -3,10 +3,10 @@ import { HostListener } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GlobalFunctionsService } from '../shared/global-functions.service';
-import { Pokemon } from '../shared/pokemon'; 
-import { RestApiService } from "../shared/rest-api.service"; 
+import { Pokemon } from '../shared/pokemon';
+import { RestApiService } from "../shared/rest-api.service";
 
-@Component({ 
+@Component({
   selector: 'app-full-pokedex',
   templateUrl: './full-pokedex.component.html',
   styleUrls: ['./full-pokedex.component.css']
@@ -40,50 +40,52 @@ export class FullPokedexComponent implements OnInit {
   }
 
   public getPokemonSpeciesDetails(pokemon: Pokemon) {
-    this.restApi.getPokemonSpeciesDetails(pokemon.dex).subscribe((data: any) => {
-      //set names
-      for (let n = 0; n < data.names.length; n++) {
-        let language: string = data.names[n].language.name;
-        let name: string = data.names[n].name;
-        name = name.charAt(0).toUpperCase() + name.slice(1);
-        switch (language) {
-          case "de":
-            pokemon.de = name;
-            break;
-          case "en":
-            pokemon.en = name;
-            break;
-          case "es":
-            pokemon.es = name;
-            break;
-          case "fr":
-            pokemon.fr = name;
-            break;
-          case "it":
-            pokemon.it = name;
-            break;
-          case "ja":
-            pokemon.ja = name;
-            break;
-          case "ko":
-            pokemon.ko = name;
-            break;
-          default:
+    if (pokemon.shiny) {
+      let dex: number = parseInt(pokemon.dex!);
+      pokemon.de = this.pokemons[dex - 1][0].de;
+      pokemon.en = this.pokemons[dex - 1][0].en;
+      pokemon.es = this.pokemons[dex - 1][0].es;
+      pokemon.fr = this.pokemons[dex - 1][0].fr;
+      pokemon.it = this.pokemons[dex - 1][0].it;
+      pokemon.ja = this.pokemons[dex - 1][0].ja;
+    } else {
+      this.restApi.getPokemonSpeciesDetails(pokemon.dex).subscribe((data: any) => {
+        //set names
+        for (let n = 0; n < data.names.length; n++) {
+          let language: string = data.names[n].language.name;
+          let name: string = data.names[n].name;
+          name = name.charAt(0).toUpperCase() + name.slice(1);
+          switch (language) {
+            case "de":
+              pokemon.de = name;
+              break;
+            case "en":
+              pokemon.en = name;
+              break;
+            case "es":
+              pokemon.es = name;
+              break;
+            case "fr":
+              pokemon.fr = name;
+              break;
+            case "it":
+              pokemon.it = name;
+              break;
+            case "ja":
+              pokemon.ja = name;
+              break;
+            case "ko":
+              pokemon.ko = name;
+              break;
+            default:
+          }
         }
-        if (pokemon.dex == "100") {
-          var pokemonCounter = document.querySelectorAll('.filter-all').length - document.querySelectorAll('.filter-all.hidden-true').length;
-          document.getElementById("pokemonCounter")!.innerText = `${pokemonCounter}`;
-        }
-      }
-    });
+      });
+    }
   }
 
   public setPokemonDetails(pokemon: Pokemon, index: number) {
     this.restApi.getPokemonDetails(pokemon.dex).subscribe((data: any) => {
-      //set types    
-      //console.log(data);
-
-
       let types: string[] = ['', '', '', '', '', ''];
       for (let n = 0; n < data.types.length; n++) {
         types[n] = data.types[n].type.name
@@ -94,7 +96,6 @@ export class FullPokedexComponent implements OnInit {
       var weight = parseFloat(data.weight) / 10;
       pokemon.weight = `${weight} kg`
       pokemon.url_front = data.sprites.front_default;
-
       //set and add shiny pokemon version
       try {
         let pokemonShiny: Pokemon = Object.assign({}, pokemon);
@@ -105,6 +106,10 @@ export class FullPokedexComponent implements OnInit {
         }
       } catch (e) {
         console.log("No shiny exists: " + pokemon.dex);
+      }
+      if (index == (this.restApi.maxFetch - 1)) {
+        var pokemonCounter = document.querySelectorAll('.filter-all').length - document.querySelectorAll('.filter-all.hidden-true').length;
+        document.getElementById("pokemonCounter")!.innerText = `${pokemonCounter}`;
       }
     });
   }
@@ -131,6 +136,7 @@ export class FullPokedexComponent implements OnInit {
       }
       for (let i = 0; i < this.pokemons.length; i++) {
         this.getPokemonSpeciesDetails(this.pokemons[i][0]);
+       
       }
       for (let i = 0; i < this.globalFunctions.pokedexRegions.length; i++) {
         this.getSpeciesByRegion(this.globalFunctions.pokedexRegions[i].url, this.globalFunctions.pokedexRegions[i].name);
@@ -172,120 +178,120 @@ export class FullPokedexComponent implements OnInit {
           url = pokemon_entry.pokemon_species.url;
           var urlAr = url.split("/");
           let dex: number = parseInt(urlAr[urlAr.length - 2]);
-          if (dex <= this.restApi.maxFetch && this.pokemons[dex-1] !== undefined) {
-            this.pokemons[dex-1][0].regions.push(regionName);
-            this.pokemons[dex-1][0].regions.splice(this.pokemons[dex-1][0].regions.indexOf(''), 1);           
-          }     
-      }
-      }); 
-  });
-}
-
-getUsersPokemons() {
-  //console.log("getUsersPokemons.");
-  this.restApi.getUsersPokemons().subscribe((data: any) => {
-    this.usersPokemons = data;
-    //console.log(this.usersPokemons);
-
-    // document.getElementById("full-pokedex-spinner")!.style.display = "none";
-    // this.pokemonCounter = data.results.length
-    // var counter: number;
-    // for (var pokemonL1 of data.results!) {
-    //   var lcPokemon: Pokemon = new Pokemon();
-    //   lcPokemon.de = pokemonL1.name;
-    //   lcPokemon.url = pokemonL1.url;
-    //   var urlAr = pokemonL1.url.split("/");
-    //   lcPokemon.dex = urlAr[urlAr.length - 2];
-    //   this.pokemons.push(lcPokemon);
-    // }
-    // for (let i = 0; i < this.pokemons.length; i++) {
-    //   this.getPokemonSpeciesDetails(this.pokemons[i]);
-    // }
-    // for (let i = 0; i < this.pokemons.length; i++) {
-    //   this.getPokemonDetails(this.pokemons[i]);
-    // }
-  });
-}
-
-
-ngOnInit(): void {
-  //document.getElementById("pokemonCounter")!.innerHTML = globalFunctions.spinnerIcon;
-
-  // var pokemon = Object();
-  // pokemon.types = [];
-  // pokemon.types.push = ["abc"];
-  // pokemon.types.push = ["efg"];
-  // this.pokemons.push(pokemon);
-
-  //this.fetchPokemonsFromPokeApi();
-  this.getAllPokemons()
-  //this.buildRegions();
-}
-
-searchByNrName(value: string): void {
-  document.querySelectorAll('.pokemons').forEach(element => element.classList.remove('pokemon-selected'));
-
-  var searchNrName: HTMLElement;
-  if(value.length == 0) {
-  alert("Bitte geben Sie den Namen oder die Nummer des Pokémon ein!")
-  return;
-}
-
-var allHidden = true;
-var selection = document.querySelectorAll(".pokemon-" + value);
-
-selection.forEach(element => {
-  if (!element.classList.contains("hidden-true")) {
-    allHidden = false;
-    element.classList.add("pokemon-selected");
-    const yOffset = -20;
-    const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-    window.scrollTo({ top: y, behavior: 'smooth' })
-  }
-  element.classList.add('pokemon-selected');
-});
-
-if (selection.length > 0) {
-  if (allHidden) {
-    alert("Der gesuchte Pokémon wird durch Ihre Filter ausgeblendet!");
-  }
-} else {
-  alert("Zu dem Namen oder der Nummer wurde keine Pokémon gefunden.")
-}
+          if (dex <= this.restApi.maxFetch && this.pokemons[dex - 1] !== undefined) {
+            this.pokemons[dex - 1][0].regions.push(regionName);
+            this.pokemons[dex - 1][0].regions.splice(this.pokemons[dex - 1][0].regions.indexOf(''), 1);
+          }
+        }
+      });
+    });
   }
 
-newSearch(): void {
-  var element = document.getElementById("searchNrName");
-  const y = element!.getBoundingClientRect().top;
-  window.scrollTo({ top: y, behavior: 'smooth' })
-    element!.focus();
-}
+  getUsersPokemons() {
+    //console.log("getUsersPokemons.");
+    this.restApi.getUsersPokemons().subscribe((data: any) => {
+      this.usersPokemons = data;
+      //console.log(this.usersPokemons);
 
-markPokemon(event: any): void {
-  if(!event.srcElement.closest(".pokemons").classList.contains('pokemon-selected')) {
-  document.querySelectorAll('.pokemons').forEach(element => element.classList.remove('pokemon-selected'));
-  event.srcElement.closest(".pokemons").classList.add('pokemon-selected');
-}
+      // document.getElementById("full-pokedex-spinner")!.style.display = "none";
+      // this.pokemonCounter = data.results.length
+      // var counter: number;
+      // for (var pokemonL1 of data.results!) {
+      //   var lcPokemon: Pokemon = new Pokemon();
+      //   lcPokemon.de = pokemonL1.name;
+      //   lcPokemon.url = pokemonL1.url;
+      //   var urlAr = pokemonL1.url.split("/");
+      //   lcPokemon.dex = urlAr[urlAr.length - 2];
+      //   this.pokemons.push(lcPokemon);
+      // }
+      // for (let i = 0; i < this.pokemons.length; i++) {
+      //   this.getPokemonSpeciesDetails(this.pokemons[i]);
+      // }
+      // for (let i = 0; i < this.pokemons.length; i++) {
+      //   this.getPokemonDetails(this.pokemons[i]);
+      // }
+    });
   }
 
-fetchPokemonData(pokemon: { url: any; }) {
-  let url = pokemon.url // <--- this is saving the pokemon url to a      variable to us in a fetch.(Ex: https://pokeapi.co/api/v2/pokemon/1/)
-  fetch(url)
-    .then(response => response.json())
-    .then((pokeData) => {
-      this.pokemons2.push(pokeData);
-    })
-}
 
-toggleShiny(pSelector: any) {
-  document.querySelectorAll(pSelector).forEach(element => {
-    if (element.classList.contains('hidden-true')) {
-      element.classList.remove('hidden-true');
-      element.classList.add('pokemon-selected');
-    } else {
-      element.classList.add('hidden-true');
+  ngOnInit(): void {
+    //document.getElementById("pokemonCounter")!.innerHTML = globalFunctions.spinnerIcon;
+
+    // var pokemon = Object();
+    // pokemon.types = [];
+    // pokemon.types.push = ["abc"];
+    // pokemon.types.push = ["efg"];
+    // this.pokemons.push(pokemon);
+
+    //this.fetchPokemonsFromPokeApi();
+    this.getAllPokemons()
+    //this.buildRegions();
+  }
+
+  searchByNrName(value: string): void {
+    document.querySelectorAll('.pokemons').forEach(element => element.classList.remove('pokemon-selected'));
+
+    var searchNrName: HTMLElement;
+    if (value.length == 0) {
+      alert("Bitte geben Sie den Namen oder die Nummer des Pokémon ein!")
+      return;
     }
-  });
-  this.globalFunctions.updateCounter(true, 400);
-}
+
+    var allHidden = true;
+    var selection = document.querySelectorAll(".pokemon-" + value);
+
+    selection.forEach(element => {
+      if (!element.classList.contains("hidden-true")) {
+        allHidden = false;
+        element.classList.add("pokemon-selected");
+        const yOffset = -20;
+        const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+        window.scrollTo({ top: y, behavior: 'smooth' })
+      }
+      element.classList.add('pokemon-selected');
+    });
+
+    if (selection.length > 0) {
+      if (allHidden) {
+        alert("Der gesuchte Pokémon wird durch Ihre Filter ausgeblendet!");
+      }
+    } else {
+      alert("Zu dem Namen oder der Nummer wurde keine Pokémon gefunden.")
+    }
+  }
+
+  newSearch(): void {
+    var element = document.getElementById("searchNrName");
+    const y = element!.getBoundingClientRect().top;
+    window.scrollTo({ top: y, behavior: 'smooth' })
+    element!.focus();
+  }
+
+  markPokemon(event: any): void {
+    if (!event.srcElement.closest(".pokemons").classList.contains('pokemon-selected')) {
+      document.querySelectorAll('.pokemons').forEach(element => element.classList.remove('pokemon-selected'));
+      event.srcElement.closest(".pokemons").classList.add('pokemon-selected');
+    }
+  }
+
+  fetchPokemonData(pokemon: { url: any; }) {
+    let url = pokemon.url // <--- this is saving the pokemon url to a      variable to us in a fetch.(Ex: https://pokeapi.co/api/v2/pokemon/1/)
+    fetch(url)
+      .then(response => response.json())
+      .then((pokeData) => {
+        this.pokemons2.push(pokeData);
+      })
+  }
+
+  toggleShiny(pSelector: any) {
+    document.querySelectorAll(pSelector).forEach(element => {
+      if (element.classList.contains('hidden-true')) {
+        element.classList.remove('hidden-true');
+        element.classList.add('pokemon-selected');
+      } else {
+        element.classList.add('hidden-true');
+      }
+    });
+    this.globalFunctions.updateCounter(true, 400);
+  }
 }
